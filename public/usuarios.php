9b847329bb580ad5607368367d108f0d34b7aee3
Guilherme731 <guilherme_moreira6@estudante.sesisenai.org.br>
2025-10-30 09:34:39 -0300

    add: estrutura HTML do cadastro de usuarios

---
+++ public/usuarios.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de Usuários</title>
+</head>
+<body>
+    <h1>Cadastrar Usuario</h1>
+    <form>
+        <label for="nome">Nome:</label>
+        <input type="text" name="nome" id="nome" required>
+        <label for="email">E-mail:</label>
+        <input type="email" name="email" id="email" required>
+        <input type="submit" value="Cadastrar">
+    </form>
+</body>
+</html>
